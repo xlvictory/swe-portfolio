7cd2ed7cb0aecc48aa5522feee6ae6887080e954
Xolani Victory <xolaniv@xolanis-imac.lan>
2024-04-02 01:11:36 -0400

    Landing page completely up, headers for About and Projects are up

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -4,7 +4,14 @@ export default function Projects() {
     return (
         <>
         <div> 
-            
+            <div id="greeting">
+                <h1><b>Tech<span>nic</span><span>al Pro</span><span>jec</span>ts⋆<span>*`</span></b></h1>
+            </div>
+        </div>
+        <div>
+            <div id="greeting">
+                <h1><b><span>Art</span>ist<span>ic</span><span> Pro</span>jects⋆<span>*`</span></b></h1>
+            </div>
         </div>
         </>
     )
